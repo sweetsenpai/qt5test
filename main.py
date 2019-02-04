1191c4f2e7193772cd809846f07f541ed64362dc
Sasha <chcolatemilk00@gmail.com>
2019-02-04 19:05:59 +0400

    Test application

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
-from PyQt5 import *
-from PyQt5.QtWidgets import *
 
-class Page(QWidget):
-    def __init__(self):
-        super(Page, self).__init__(parent=None)
-
-        my_label = QLabel("This is my label")
-        layout = QVBoxLayout()
-
-        layout.addWidget(my_label)
-
-        mainLayout = QGridLayout()
-        mainLayout.addLayout(layout,0,1)
-
-        self.setLayout(mainLayout)
-        self.setWindowTitle("My first pyQT app")
-
-
+import os,sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtQuick import *
+from PyQt5.Qt import *
 
 if __name__ == '__main__':
-    import sys
-
+    # создаём экземпляр приложения
     app = QApplication(sys.argv)
-
-    window = Page()
-    window.show()
-
+    # создаем qml движок
+    engine = QQmlApplicationEngine()
+    # загружаем файл qml в движок
+    engine.load(QUrl.fromLocalFile("main.qml"))
     sys.exit(app.exec_())
-
-
-
-
-
-
