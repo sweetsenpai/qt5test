6866baff1ce0ebbaa7900b25a5b7d2dea0562f42
Sasha <chcolatemilk00@gmail.com>
2019-02-05 00:21:12 +0400

    Куки кликер

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QApplication, QHBoxLayout,
+                            QVBoxLayout, QPushButton, QLabel, QLineEdit)
 
-import os,sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtQuick import *
-from PyQt5.Qt import *
 
-if __name__ == '__main__':
-    # создаём экземпляр приложения
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+        self.counter = 0
+
+    def init_ui(self):
+        label = QLabel("NAME: ")
+        name_input = QLineEdit()
+        self.button = QPushButton("Clicked: 0")
+        # button.clicked.connect(self.click)
+        self.button.pressed.connect(self.press)
+        self.button.released.connect(self.click)
+
+        h= QHBoxLayout()
+        #h.addStretch(1)
+        h.addWidget(label)
+        h.addWidget(name_input)
+
+        v = QVBoxLayout()
+        #v.addStretch(1)
+        v.addLayout(h)
+        v.addWidget(self.button)
+
+        self.setLayout(v)
+        self.setWindowTitle("Horizontal Layout")
+        self.show()
+
+    def click(self):
+        print("THIS BUTTON HAS BEEN REALISED!!!")
+
+    def press(self):
+        print("THIS BUTTON HAS BEEN PRESSED!!!")
+        self.counter += 1
+        self.button.setText("Clicked: " + str(self.counter))
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    # создаем qml движок
-    engine = QQmlApplicationEngine()
-    # загружаем файл qml в движок
-    engine.load(QUrl.fromLocalFile("main.qml"))
+    window = MainWindow()
     sys.exit(app.exec_())
+
+
